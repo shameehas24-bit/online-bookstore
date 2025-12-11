58cebfb8276cdf1edf3b5f74bf37b43065b2533c
Shameeha S <shameehas.24@gmail.com>
2025-12-11 10:31:39 +0530

    Create CartItem.java

---
+++ src/main/java/com/bookstore/entity/CartItem.java
@@ -0,0 +1,21 @@
+package com.bookstore.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Book book;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private int quantity;
+}
