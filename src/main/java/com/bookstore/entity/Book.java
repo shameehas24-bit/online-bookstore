9124e9f50292337d6ed26ed90ae213f3cb76aa8b
Shameeha S <shameehas.24@gmail.com>
2025-12-11 10:29:37 +0530

    Create Book.java

---
+++ src/main/java/com/bookstore/entity/Book.java
@@ -0,0 +1,15 @@
+package com.bookstore.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private double price;
+}
