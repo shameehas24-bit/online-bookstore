ca82f467fff1d7e2c84378ce6b07deb1e03d4e86
Shameeha S <shameehas.24@gmail.com>
2025-12-11 10:32:02 +0530

    Create BookRepository.java

---
+++ src/main/java/com/bookstore/repository/BookRepository.java
@@ -0,0 +1,6 @@
+package com.bookstore.repository;
+
+import com.bookstore.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> { }
